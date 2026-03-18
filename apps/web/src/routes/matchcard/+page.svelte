506865f251f8d7e5b81040f4e4530e8a3e9ecbe9
quantm103@gmail.com <quantm103@gmail.com>
2026-03-18 16:51:12 +0700

    Feat: add template matchcard

---
+++ apps/web/src/routes/matchcard/+page.svelte
@@ -0,0 +1,37 @@
+
+
+
+<script lang="ts">
+	import Card from "./Card.svelte";
+import { onMount } from 'svelte';
+
+  let curPage = $state(1);
+  let flashcards = $state<any[]>([]);
+  let hasMore = $state(true);
+  let isLoading = $state(false);
+
+  const fetchData = async () => {
+    if (isLoading || !hasMore) return;
+    isLoading = true;
+    try {
+      const res = await fetch(`/api/flashcard?page=${curPage}`);
+      if (res.ok) {
+        const { items, hasMore: more } = await res.json();
+        flashcards = [...flashcards, ...items];
+        hasMore = more;
+        curPage++;
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      isLoading = false;
+    }
+  };
+
+ onMount(() => {
+   fetchData();
+ })
+ $inspect(flashcards)
+</script>
+
+<Card cards={flashcards} />
